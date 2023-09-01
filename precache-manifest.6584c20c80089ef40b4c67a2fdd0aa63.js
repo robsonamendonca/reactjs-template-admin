26b8f2798cb6b6d619fc8dad48471ead3f78478b
Robson Mendonça <rob.ams@gmail.com>
2023-08-31 23:41:34 -0300

    deploy

---
+++ precache-manifest.6584c20c80089ef40b4c67a2fdd0aa63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b17084d8b901c399db8e",
+    "url": "/static/css/main.bff4bc7f.chunk.css"
+  },
+  {
+    "revision": "b17084d8b901c399db8e",
+    "url": "/static/js/main.b17084d8.chunk.js"
+  },
+  {
+    "revision": "256afa844109bbc99318",
+    "url": "/static/js/1.256afa84.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "174545e5205555c386cfa03af2f289cf",
+    "url": "/index.html"
+  }
+];
